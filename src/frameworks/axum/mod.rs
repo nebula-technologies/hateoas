097143c02274cc72658329d4dfd9e7a509cbb14f
Anders Blenstrup-Pedersen <abp-git@ryuu.technology>
2022-12-08 03:10:35 +0900

    update

--- src/frameworks/axum/mod.rs
+++ src/frameworks/axum/mod.rs
@@ -1,7 +1,42 @@
-// use crate::Payload;
-// use axum::response::{IntoResponse, Response};
-// use serde::de::DeserializeOwned;
+use crate::{Hateoas, HateoasResource};
+use axum::extract::FromRequest;
+use axum::http::Request;
+use axum::response::{IntoResponse, Response, ResponseParts};
+use axum_core::extract::FromRequestParts;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::future::Future;
+use std::pin::Pin;
 
-// impl<T: Serialize + DeserializeOwned> IntoResponse for Payload<T> {
-//     fn into_response(self) -> Response {}
+impl<T: Serialize + HateoasResource> IntoResponse for Hateoas<T> {
+    fn into_response(self) -> Response {
+        ().into_response()
+    }
+}
+
+// impl<T: DeserializeOwned + HateoasResource, S, B> FromRequest<S, B> for Hateoas<T> {
+//     type Rejection = ();
 // }
+
+impl<S, B, T: DeserializeOwned + HateoasResource> FromRequest<S, B> for Hateoas<T>
+where
+    B: Send + 'static,
+    S: Send + Sync,
+    T: FromRequestParts<S>,
+{
+    type Rejection = Hateoas<()>;
+
+    fn from_request<'a, 'b>(
+        req: Request<B>,
+        state: &'a S,
+    ) -> Pin<Box<dyn Future<Output = Result<Self, Self::Rejection>> + Send + 'b>>
+    where
+        'a: 'b,
+        Self: 'b,
+    {
+        Box::pin(async move {
+            req.headers()
+
+            Ok(Hateoas::OK(None, None)) })
+    }
+}
